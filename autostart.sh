87c9aa52314e84dd32a3ef2300652b79f48269c6
jggc <jeangabriel.gc@gmail.com>
2014-06-23 12:03:42 -0400

    Create autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+nitrogen --restore &
+avant-window-navigator &
+xscreensaver & // start screen saver daemon
+/usr/lib/notification-daemon-1.0/notification-daemon &
+xcompmgr &
+pulseaudio &
+mpd &
+conky &
+setxkbmap ca,fr 
+xmodmap /home/jeangab/git/stuff/speedswap 
